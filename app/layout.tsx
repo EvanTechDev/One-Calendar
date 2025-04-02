2e9bf6b8ec353f71e2c24e0a0d8933a3a6e6c859
Evan Huang <evan.huang000@proton.me>
2025-04-02 22:11:01 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { CalendarProvider } from "@/contexts/CalendarContext"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "One Calendar",
-  description: "A calendar application",
+  title: "One Calendar | Customize your day at your fingertips and save as much time as you want.",
+  description: "One Calendar is a calendar web app that uses React + Vercel/blob for storage. It has rich features, such as: address book, notes, bookmarks, to-do lists and analysis features!"",
 }
 
 export default function RootLayout({
