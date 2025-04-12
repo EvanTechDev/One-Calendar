0efca3a30699e460fae699039cccd16da13c8983
Evan Huang <evan.huang000@proton.me>
2025-04-12 17:46:18 +0800

    Update UserProfileButton.tsx

--- components/UserProfileButton.tsx
+++ components/UserProfileButton.tsx
@@ -17,7 +17,7 @@ import { validatePassword, generateIdFromPassword } from "@/lib/backup-utils"
 import { useCalendar } from "@/contexts/CalendarContext"
 import { translations, useLanguage } from "@/lib/i18n"
 import { Checkbox } from "@/components/ui/checkbox"
-import { useUser, SignInButton, SignUpButton, SignOutButton } from "@clerk/nextjs";
+import { useUser, SignIn, SignUp, SignOutButton } from "@clerk/nextjs";
 
 export default function UserProfileButton() {
   const [language] = useLanguage()
