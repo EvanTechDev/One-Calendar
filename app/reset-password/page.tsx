db7cc28291acbf1a4a04c2bf96b3e7d07da13fa1
Evan Huang <evan.huang000@proton.me>
2025-04-13 18:22:40 +0800

    Update page.tsx

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,8 +2,8 @@ import { ResetPasswordForm } from "@/components/ResetForm";
 
 export default function ResetPasswordPage() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <ResetPasswordForm />
+    <div className="container flex h-screen flex-col items-center justify-center">
+      <ResetPasswordForm className="w-full max-w-md" />
     </div>
   );
 }
