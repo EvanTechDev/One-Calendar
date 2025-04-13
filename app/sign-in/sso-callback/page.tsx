709dca540dae22fa1ae8b648180084c1e22a65b9
Evan Huang <evan.huang000@proton.me>
2025-04-13 10:22:59 +0800

    Update page.tsx

--- app/sign-in/sso-callback/page.tsx
+++ app/sign-in/sso-callback/page.tsx
@@ -1,5 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useAuth } from '@clerk/nextjs';
+import { useSearchParams } from 'next/navigation';
+
 export default function SSOSignInCallback() {
+  const { isLoaded, isSignedIn } = useAuth();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) {
+      const redirect = searchParams.get('redirect_url') || '/';
+      window.location.href = redirect;
+    }
+  }, [isLoaded, isSignedIn, searchParams]);
+
   return null;
 }
