2e487fe5b7891406a1803ffd34fe94f7bfc801c0
Evan Huang <evan.huang000@proton.me>
2025-04-14 23:35:07 +0800

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
         <div className="absolute right-1/4 bottom-1/3 h-[250px] w-[250px] rounded-full bg-indigo-400 opacity-20 blur-[90px]" />
       </div>
       <div className="relative z-10 flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium text-gray-800">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
           <Calendar className="size-4" color="#0066ff" />
           One Calendar
         </a>
