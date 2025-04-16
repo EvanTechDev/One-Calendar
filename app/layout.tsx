849314f9f90f1b2cdaf860d8f9335d7254e1c1f3
Evan Huang <evan.huang000@proton.me>
2025-04-16 23:37:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           <Toaster />
           <SpeedInsights />
         </CalendarProvider> 
-        </ThemeProvider>>
+        </ThemeProvider>
       </ClerkProvider>
       <Analytics />
       </body>
