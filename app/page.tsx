2780da21aca8b7d47b7d3ca0aef30d0fd5f7e73b
Evan Huang <evan.huang000@proton.me>
2025-04-18 23:15:14 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
 import Calendar from "@/components/Calendar"
-import type { Metadata } from "next"
-
-export const metadata: Metadata = {
-  title: "One Calendar | Get organized and make the most of your time",
-  description: "One Calendar is a calendar web app that uses React + Vercel/blob for storage. It has rich features: address book, notes, bookmarks, to-do lists and analysis features!",
-}
 
 export default function Home() {
   return <Calendar />
