9d59d20fe6f2330c502fd4b7b4c895aeb366c6bc
Evan Huang <evan.huang000@proton.me>
2025-04-19 17:21:12 +0800

    Update WeekView.tsx

--- components/WeekView.tsx
+++ components/WeekView.tsx
@@ -470,7 +470,7 @@ export default function WeekView({
 
                   return (
                     <div
-                      className="absolute left-0 right-0 border-t-2 border-[#0066FF] z-10"
+                      className="absolute left-0 right-0 border-t-2 border-[#0066FF] z-0"
                       style={{
                         top: `${topPosition}px`,
                       }}
