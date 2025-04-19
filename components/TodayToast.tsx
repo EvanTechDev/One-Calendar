6921e9f7bb025f62d0539b516767c06254e5a482
Evan Huang <evan.huang000@proton.me>
2025-04-19 21:53:09 +0800

    Update TodayToast.tsx

--- components/TodayToast.tsx
+++ components/TodayToast.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { toast } from 'sonner';
+import { toast } from '@/components/ui/use-toast';
 import { Button } from '@/components/ui/button';
 
 const getLocale = () => {
