edcae6972c73b37afaf9ed2d6af14dbef385417c
Evan Huang <evan.huang000@proton.me>
2025-04-20 14:20:42 +0800

    Update page.tsx

--- app/test-sign-in/page.tsx
+++ app/test-sign-in/page.tsx
@@ -8,7 +8,7 @@ export default function SignInPage() {
     <div className="flex items-center justify-center min-h-screen bg-muted">
       <Card className="w-full max-w-md shadow-xl border-none">
         <CardContent className="p-6">
-          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+          <SignIn signUpUrl="/sign-up" />
         </CardContent>
       </Card>
     </div>
