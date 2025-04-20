9618e5c258e6242973535597e0b41aa0ab38efde
Evan Huang <evan.huang000@proton.me>
2025-04-20 08:18:09 +0800

    Create CalendarIcon.tsx

---
+++ components/icons/CalendarIcon.tsx
@@ -0,0 +1,13 @@
+import { Calendar } from 'lucide-react';
+import { motion } from 'motion/react';
+import { AnimatedIconWrapper, iconVariants } from './AnimatedIconWrapper';
+
+export const CalendarIcon = forwardRef((props, ref) => (
+  <AnimatedIconWrapper ref={ref} {...props}>
+    <motion.div variants={iconVariants} animate="normal">
+      <Calendar className="w-5 h-5" />
+    </motion.div>
+  </AnimatedIconWrapper>
+));
+
+CalendarIcon.displayName = 'CalendarIcon';
