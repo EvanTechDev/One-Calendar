6b7d99e55367c8a8869956446820beed9f9b256f
Evan Huang <evan.huang000@proton.me>
2025-04-24 21:41:55 +0800

    Update page.tsx

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -2,11 +2,22 @@
 
 import { useEffect, useState } from "react";
 import Calendar from "@/components/Calendar";
-import { useIsMobile } from "@/hooks/useMobile";
 
 export default function Home() {
-  const isMobile = useIsMobile();
+  const [isMobile, setIsMobile] = useState(false);
   const [isChinese, setIsChinese] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkIfMobile();
+
+    window.addEventListener("resize", checkIfMobile);
+
+    return () => window.removeEventListener("resize", checkIfMobile);
+  }, []);
 
   useEffect(() => {
     const lang = navigator.language;
