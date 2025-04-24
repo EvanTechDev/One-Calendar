bdced33853df6e51df4655eabf6e4fb1bfdc0281
Evan Huang <evan.huang000@proton.me>
2025-04-24 21:19:35 +0800

    fix: update

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -1,20 +1,21 @@
+import { useEffect, useState } from "react";
 import Calendar from "@/components/Calendar";
 import { useIsMobile } from "@/hooks/use-mobile";
-import React, { useState } from "react"
 
 export default function Home() {
   const isMobile = useIsMobile();
-  const language = navigator.language;
+  const [isChinese, setIsChinese] = useState(false);
+
+  useEffect(() => {
+    const lang = navigator.language;
+    setIsChinese(lang.startsWith("zh"));
+  }, []);
 
   return (
     <div>
       {isMobile ? (
         <div>
-          {language.startsWith("zh") ? (
-            <p>请使用电脑打开此页面</p>
-          ) : (
-            <p>Please open this page on a computer</p>
-          )}
+          <p>{isChinese ? "请使用电脑打开此页面" : "Please open this page on a computer"}</p>
         </div>
       ) : (
         <Calendar />
