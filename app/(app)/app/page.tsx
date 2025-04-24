4aae39b9e2bafa01f528573f5b888c6b653f4976
Evan Huang <evan.huang000@proton.me>
2025-04-24 21:15:18 +0800

    fix: deploy bug

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -1,24 +1,18 @@
-import * as React from 'react'
-import Calendar from "@/components/Calendar"
-import { useIsMobile } from "@/hooks/use-mobile"
+import Calendar from "@/components/Calendar";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Home() {
   const isMobile = useIsMobile();
-  const userLanguage = navigator.language || navigator.userLanguage;
-
-  const isChinese = userLanguage.startsWith('zh');
-  const isEnglish = userLanguage.startsWith('en');
+  const language = navigator.language;
 
   return (
     <div>
       {isMobile ? (
         <div>
-          {isChinese ? (
+          {language.startsWith("zh") ? (
             <p>请使用电脑打开此页面</p>
-          ) : isEnglish ? (
-            <p>Please open this page on a computer</p>
           ) : (
-            <p>请使用电脑打开此页面。/ Please open this page on a computer.</p>
+            <p>Please open this page on a computer</p>
           )}
         </div>
       ) : (
