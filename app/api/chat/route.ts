c5a7e1b001070aacd655c7b23e09f403dc4f32a9
Evan Huang <evan.huang000@proton.me>
2025-04-27 21:15:32 +0800

    fix: object bug

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,23 +1,19 @@
 import { groq } from '@ai-sdk/groq';
 import { streamText } from 'ai';
-import { createStreamableValue } from 'ai/rsc';
 import { NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-  const latestUserMessage = messages[messages.length - 1].content;
-
+  
   try {
     const result = await streamText({
       model: groq('deepseek-r1-distill-llama-70b'),
       messages,
     });
 
-    const stream = createStreamableValue(result.textStream);
-    
-    return new Response(stream.value, {
+    return new Response(result.toAIStream(), {
       headers: {
         'Content-Type': 'text/plain',
       },
