567a4a2339cb039aeaa4069b9d25c4f761836db8
Evan Huang <evan.huang000@proton.me>
2025-05-04 20:16:20 +0800

    chore: toaster to sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/sonner"
 import { CalendarProvider } from "@/components/context/CalendarContext"
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
