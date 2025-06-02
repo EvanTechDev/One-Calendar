661c1e453ff326197ed76bde3931296bb2b715bc
Evan Huang <evan.huang000@proton.me>
2025-06-02 18:22:21 +0800

    update legal

--- components/account/SignUpForm.tsx
+++ components/account/SignUpForm.tsx
@@ -288,8 +288,8 @@ export function SignUpForm({
 
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
         By continuing, you agree to our{" "}
-        <a href="https://xyehr.cn/legal/terms-of-use">Terms of Service</a> and{" "}
-        <a href="https://xyehr.cn/legal/privacy-policy">Privacy Policy</a>.
+        <a href="/terms">Terms of Service</a> and{" "}
+        <a href="/privacy">Privacy Policy</a>.
       </div>
     </div>
   );
