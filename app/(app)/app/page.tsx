55a7519c1702c0e98ca9092df09757c4c70ab86a
Evan Huang <evan.huang000@proton.me>
2025-06-12 22:07:48 +0800

    update

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -21,35 +21,7 @@ export default function Home() {
 
   {/*if (isMobile) {
     return (
-      <div>
-        <div
-          style={{
-            background: "#fff",
-            borderRadius: "24px",
-            boxShadow: "0 8px 32px rgba(60,70,130,0.12), 0 1.5px 4px rgba(80,90,210,0.09)",
-            padding: "36px 24px",
-            maxWidth: "350px",
-            width: "90vw",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "18px"
-          }}
-        >
-          <p style={{
-            fontSize: "1.15rem",
-            fontWeight: 600,
-            color: "#444d63",
-            textAlign: "center",
-            margin: 0,
-            lineHeight: 1.5
-          }}>
-            {isChinese
-              ? "请使用电脑访问此页面，以获得最佳体验"
-              : "Please open this page on a computer for best experience"}
-          </p>
-        </div>
-      </div>
+      
     )
   }*/}
 
