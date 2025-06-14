8d1e4cdaabf2cc83e1b5c474888d73e2c5b9dde2
Evan Huang <evan.huang000@proton.me>
2025-06-14 13:12:17 +0800

    fix

--- app/(app)/share/[id]/page.tsx
+++ app/(app)/share/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useParams } from "next/navigation"
-import SharedEventView from "@/components/SharedEvent"
+import SharedEventView from "@/components/home/SharedEvent"
 
 export default function SharePage() {
   const params = useParams()
