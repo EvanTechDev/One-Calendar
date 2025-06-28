f9b4f30c501b7c31811108b14626166dd0c61aa1
Evan Huang <evan.huang000@proton.me>
2025-06-28 13:37:05 +0800

    style: add purple

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -107,12 +107,12 @@ export default function Sidebar({
     )}>
       <div className="p-4">
         <div className="flex items-center mb-4">
-          <CalendarIcon className="h-6 w-6 text-[#0066ff] mr-2 green:text-[#24a854]" />
+          <CalendarIcon className="h-6 w-6 text-[#0066ff] mr-2 green:text-[#24a854] purple:text-[#6c29e0]" />
           <h1 className="text-lg font-semibold">{t.oneCalendar}</h1>
         </div>
 
         <Button
-          className="w-full justify-center bg-[#0066FF] text-white hover:bg-[#0052CC] mb-4 h-10 green:bg-[#24a854] "
+          className="w-full justify-center bg-[#0066FF] text-white hover:bg-[#0052CC] mb-4 h-10 green:bg-[#24a854] purple:bg-[#6c29e0]"
           onClick={onCreateEvent}
         >
           {t.createEvent}
