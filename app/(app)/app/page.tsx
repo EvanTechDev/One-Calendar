1fda62862a72f78554bb0007a1012f3bd59d39b3
Evan Huang <evan.huang000@proton.me>
2025-06-28 20:59:43 +0800

    fix: page render

--- app/(app)/app/page.tsx
+++ app/(app)/app/page.tsx
@@ -1,10 +1,13 @@
 "use client"
+
 import { useEffect, useState } from "react";
 import Calendar from "@/components/Calendar";
+import { useTheme } from "next-themes";
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
   const [isChinese, setIsChinese] = useState(false);
+  const { theme } = useTheme();
 
   useEffect(() => {
     const checkIfMobile = () => {
@@ -14,16 +17,29 @@ export default function Home() {
     window.addEventListener("resize", checkIfMobile);
     return () => window.removeEventListener("resize", checkIfMobile);
   }, []);
+
   useEffect(() => {
     const lang = navigator.language;
     setIsChinese(lang.startsWith("zh"));
   }, []);
 
-  {/*if (isMobile) {
-    return (
-      
-    )
-  }*/}
+  useEffect(() => {
+    const body = document.body;
+    const colorThemes = ['blue', 'green', 'purple', 'orange'];
+    
+    body.classList.add('app');
+    
+    if (theme && colorThemes.includes(theme)) {
+      body.classList.add(theme);
+    }
+
+    return () => {
+      body.classList.remove('app');
+      colorThemes.forEach(colorTheme => {
+        body.classList.remove(colorTheme);
+      });
+    };
+  }, [theme]);
 
   return <Calendar />;
 }
