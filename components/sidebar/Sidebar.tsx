2349dfdc8442d04ab3900ef91175488b0b342dba
Evan Huang <evan.huang000@proton.me>
2025-07-04 14:07:36 +0800

    add azalea

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -107,12 +107,12 @@ export default function Sidebar({
     )}>
       <div className="p-4">
         <div className="flex items-center mb-4">
-          <CalendarIcon className="h-6 w-6 text-[#0066ff] mr-2 green:text-[#24a854] purple:text-[#6c29e0] orange:text-[#e26912]" />
+          <CalendarIcon className="h-6 w-6 text-[#0066ff] mr-2 green:text-[#24a854] orange:text-[#e26912] azalea:text-[#CD2F7B]" />
           <h1 className="text-lg font-semibold">{t.oneCalendar}</h1>
         </div>
 
         <Button
-          className="w-full justify-center bg-[#0066FF] text-white hover:bg-[#0052CC] mb-4 h-10 green:bg-[#24a854] purple:bg-[#6c29e0] orange:bg-[#e26912]"
+          className="w-full justify-center bg-[#0066FF] text-white hover:bg-[#0052CC] mb-4 h-10 green:bg-[#24a854] orange:bg-[#e26912] azalea:bg-[#CD2F7B]"
           onClick={onCreateEvent}
         >
           {t.createEvent}
