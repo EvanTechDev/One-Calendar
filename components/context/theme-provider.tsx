8a9c1a789a91c43f540709bb4066af4f146e9db5
Evan Huang <evan.huang000@proton.me>
2025-07-07 15:14:14 +0800

    Crimson

--- components/context/theme-provider.tsx
+++ components/context/theme-provider.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({
   children,
-  themes = ['light', 'dark', 'green', "orange", "azalea", "pink"],
+  themes = ['light', 'dark', 'green', "orange", "azalea", "pink", "crimson"],
   ...props
 }: React.ComponentProps<typeof NextThemesProvider> & {
   themes?: string[];
