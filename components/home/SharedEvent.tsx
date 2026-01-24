f98888d3a107c61e0af88c37053f6e1e35749dca
Evan Huang <evan.huang000@proton.me>
2026-01-24 15:29:58 +0800

    fix

--- components/home/SharedEvent.tsx
+++ components/home/SharedEvent.tsx
@@ -1,6 +1,6 @@
 
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 import { zhCN, enUS } from "date-fns/locale";
