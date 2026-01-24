2e202aaa571d1fda71ad045b37acded2451e60dc
Evan Huang <evan.huang000@proton.me>
2026-01-24 15:34:26 +0800

    fix

--- components/home/SharedEvent.tsx
+++ components/home/SharedEvent.tsx
@@ -30,7 +30,7 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import FriendlyCaptchaWidget from "@/components/home/FCWidget";
+import FriendlyCaptchaWidget,  { FriendlyCaptchaRef } from "@/components/home/FCWidget";
 
 interface SharedEvent {
   id: string;
