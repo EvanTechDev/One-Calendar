ac0264ce9d7968c5965e889d19af2a7eb5a32c87
Evan Huang <evan.huang000@proton.me>
2026-02-26 23:41:31 +0800

    style: set event preview overlay to bg-black/10

--- components/app/event/event-preview.tsx
+++ components/app/event/event-preview.tsx
@@ -462,7 +462,7 @@ export default function EventPreview({
     <>
       {!shareOnlyMode && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/10"
           onClick={() => onOpenChange(false)}
         >
           <div
