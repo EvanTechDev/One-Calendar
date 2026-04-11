b4f09e89f6b93049fc2793f44ba85cab26c82890
Evan Huang <evan.huang000@proton.me>
2026-04-12 07:51:32 +0800

    fix: remove unused import

--- app/(auth)/sign-in/sso-callback/page.tsx
+++ app/(auth)/sign-in/sso-callback/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
-import { useEffect } from 'react';
 
 export default function SSOSignInCallback() {
   return <AuthenticateWithRedirectCallback />;
