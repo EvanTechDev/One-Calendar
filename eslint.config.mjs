dd713a1b29299862f1614befaeb10b585b1530d5
Evan Huang <evan.huang000@proton.me>
2026-04-16 21:43:14 +0800

    feat: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import next from 'eslint-config-next'
+import tseslint from 'typescript-eslint'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...next(),
+  {
+    rules: {
+      "no-console": "warn",
+      "prefer-const": "error"
+    }
+  }
+]
