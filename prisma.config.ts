940d31eb030dcbde63480a57021981e1627c379d
Evan Huang <evan.huang000@proton.me>
2026-04-25 16:33:47 +0800

    fix: prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   schema: 'schema.prisma',
+  datasource: {
+    url: process.env.POSTGRES_URL!,
+    directUrl: process.env.POSTGRES_URL_NON_POOLING!,
+  },
 })
