6f5f6724c6eb7eb0c1e6cffbf948968a8057fc91
Evan Huang <evan.huang000@proton.me>
2026-05-01 14:20:46 +0800

    fix: lazy-load prisma accelerate extension to avoid build-time init errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,31 @@
 import { PrismaClient } from '@/lib/generated/prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { APP_CONFIG } from '@/lib/config'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
 function createPrismaClient() {
   const client = new PrismaClient({
-    ...(APP_CONFIG.prisma.enableAccelerate ? { accelerateUrl: process.env.POSTGRES_URL } : {}),
+    ...(APP_CONFIG.prisma.enableAccelerate
+      ? { accelerateUrl: process.env.POSTGRES_URL }
+      : {}),
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   })
-  return APP_CONFIG.prisma.enableAccelerate ? client.$extends(withAccelerate()) : client
+
+  if (!APP_CONFIG.prisma.enableAccelerate) {
+    return client
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { withAccelerate } = require('@prisma/extension-accelerate') as {
+    withAccelerate: () => Parameters<PrismaClient['$extends']>[0]
+  }
+
+  return client.$extends(withAccelerate()) as PrismaClient
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
