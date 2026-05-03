d1d9b0e8b5f04d8b57da4ca2474995f325e8a987
Evan Huang <evan.huang000@proton.me>
2026-05-03 16:56:09 +0800

    fix: update middleware to improve better auth

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,17 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+import { getSessionCookie } from 'better-auth/cookies'
 
-export default function proxy(_req: NextRequest) {
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request)
+  const { pathname } = request.nextUrl
+
+  if (sessionCookie && ['/sign-in', '/sign-up'].includes(pathname)) {
+    return NextResponse.redirect(new URL('/app', request.url))
+  }
+
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/sign-in', '/sign-up'],
 }
