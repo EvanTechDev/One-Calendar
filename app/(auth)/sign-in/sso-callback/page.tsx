467a993c8dab95ee13ddf9f86e4257bdf99bede8
Evan Huang <evan.huang000@proton.me>
2026-05-05 14:09:07 +0800

    fix: remove unused page

--- app/(auth)/sign-in/sso-callback/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function SSOSignInCallback() {
-  redirect('/app')
-}
--- app/(auth)/sign-up/sso-callback/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function SSOSignUpCallback() {
-  redirect('/app')
-}
