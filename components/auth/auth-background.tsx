86d14915f39b7fe7bb02ab45ab9e8e2b6fb2bcac
Evan Huang <evan.huang000@proton.me>
2026-05-30 08:18:40 +0800

    fix: background

--- components/auth/auth-background.tsx
+++ components/auth/auth-background.tsx
@@ -1,25 +1,10 @@
 'use client'
 
 import Dither from '@/components/landing/dither'
-import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 export function AuthBackground() {
-  const { resolvedTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return <div className="fixed inset-0 -z-10 bg-white dark:bg-black" />
-  }
-
   return (
-    <div
-      style={{ width: '100%', height: '600px', position: 'relative' }}
-    >
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
       <Dither
         waveColor={[0.5, 0.5, 0.5]}
         disableAnimation={false}
