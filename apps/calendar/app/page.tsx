fb9b59c266d385151bb5a47bc28e9120e8376a7a
Evan Huang <evan.huang000@proton.me>
2026-06-26 19:22:35 +0800

    fix: use the correct global css

--- apps/calendar/app/page.tsx
+++ apps/calendar/app/page.tsx
@@ -4,10 +4,11 @@ import { LogosSection } from "@/components/landing/logos-section";
 import { FeatureSection } from "@/components/landing/feature-section";
 import { CallToAction } from "@/components/landing/cta";
 import { Footer } from "@/components/landing/footer";
+import styles from "./page.module.css"
 
 export default function LandingPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center">
+    <div className={`${styles.page} flex min-h-screen flex-col items-center`}>
       <Header />
       <main className="flex w-full flex-col gap-20">
         <HeroSection />
