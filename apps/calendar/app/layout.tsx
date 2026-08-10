09aa8ca098a95a7a62e49018a8d712599b5aa400
evantechdev <evantechdev@users.noreply.github.com>
2026-08-10 12:07:55 +0800

    fix: inline sidebar logo svg to eliminate network request

--- apps/calendar/app/layout.tsx
+++ apps/calendar/app/layout.tsx
@@ -50,9 +50,6 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn('font-sans', inter.variable, geistHeading.variable)}
     >
-      <head>
-        <link rel="preload" href="/icon.svg" as="image" />
-      </head>
       <body
         className={`${GeistSans.className} ${instrumentSans.variable} antialiased`}
       >
